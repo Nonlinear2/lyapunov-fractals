1490c598828d025f6134bd40e5392f68552835cf
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2024-05-28 11:46:26 +0200

    improved default fractal colors

--- generate_fractal_image.py
+++ generate_fractal_image.py
@@ -4,7 +4,7 @@ from random import randint
 
 def main():
     fractal_computer = ComputeFractals(pattern="yxxzxx", x_min = 3.43, x_max = 3.95, 
-                                       y_min = 2.84, y_max = 3.36, size=2000)
+                                       y_min = 2.84, y_max = 3.36, size=2000, color_resolution=1900)
     image = fractal_computer.compute_fractal(1.57)
 
     image = Image.fromarray(np.swapaxes(image.astype(np.uint8), 0, 1))
